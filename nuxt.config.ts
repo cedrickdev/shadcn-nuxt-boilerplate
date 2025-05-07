7751079a5efedf09824c4bef56c19d51db9d0cd1
Cedrick Feze <cedrickv.tchindaf@hes-so.ch>
2025-05-07 20:57:23 +0200

    feat: update app layout and integrate Tailwind CSS
    - Replaced <NuxtWelcome /> with <NuxtPage /> in app.vue.
    - Added Tailwind CSS configuration in nuxt.config.ts.
    - Included several new dependencies for Tailwind CSS support and other components.
    - Updated package.json and pnpm-lock.yaml to reflect new dependencies.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: ["shadcn-nuxt"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+  css: ["~/assets/css/tailwind.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
